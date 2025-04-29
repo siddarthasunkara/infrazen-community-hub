a666ebef98e82f51565a2ae20fd3f7d2744d5072
SIDDARTHA SUNKARA <101887416+siddarthasunkara@users.noreply.github.com>
2025-04-29 21:38:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,14 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",  // Explicitly define the output directory as 'dist'
   },
 }));
